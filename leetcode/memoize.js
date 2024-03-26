a34a7d39eb2244c14b90a0ed10ddeeffbe7f13cf
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-03-26 02:35:05 -0700

    update memoize.js

--- leetcode/memoize.js
+++ leetcode/memoize.js
@@ -22,7 +22,7 @@ function memoize(fn) {
       if (lastValue !== result) {
         result = fn(...args);
         lastValue = result;
-        return fn(...args);
+        return result;
       } else {
         return lastValue;
       }
@@ -39,3 +39,90 @@ function memoize(fn) {
  * memoizedFn(2, 3) // 5
  * console.log(callCount) // 1 
  */
+
+/* 
+Approach
+To memoize a function, we can create a cache object that stores the results of previous function calls. 
+Whenever the function is called, we check if the cache already has a result for the given input parameters. 
+If it does, we return the cached result. Otherwise, we compute the result and store it in the cache for future use.
+
+To implement memoization in JavaScript/TypeScript, we can create a higher-order function that takes in the original 
+function and returns a memoized version of it. The memoized function can use a closure to store the cache object.
+
+Complexity
+Time complexity:
+The time complexity of the memoized function is O(1) for each call if the cache hit rate is high. In the worst case, 
+where all input parameters are unique, the memoized function will perform the same number of function calls as the original function.
+
+Space complexity:
+The space complexity of the memoized function depends on the number of unique input parameters and the size of the 
+cache object. If the number of unique input parameters is small and the cache object is not too large, 
+then the space complexity will be reasonable. However, if the number of unique input parameters is very large 
+and/or the cache object is very large, then the space complexity can become a concern.
+
+
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      const key = String(args);
+      if (key in cache) {
+        return cache[key];
+      }
+      const result = fn(...args);
+      cache[key] = result;
+      return result;
+    }
+}
+
+
+// Fibonacci function
+const fib = (n) => (n <= 1 ? 1 : fib(n - 1) + fib(n - 2));
+
+// Factorial function
+const factorial = (n) => (n <= 1 ? 1 : factorial(n - 1) * n);
+
+
+
+There are other ways to implement memoization in JavaScript. Here are a few:
+
+Using a Map::
+function memoize(fn) {
+  const cache = new Map();
+  return function(...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  }
+}
+Using function properties:
+function memoize(fn) {
+  fn.cache = {};
+  return function(...args) {
+    const key = JSON.stringify(args);
+    if (key in fn.cache) {
+      return fn.cache[key];
+    }
+    const result = fn(...args);
+    fn.cache[key] = result;
+    return result;
+  }
+}
+
+Using closures::
+function memoize(fn) {
+  const cache = {};
+  return function memoized(...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  }
+}
+*/
