93076294627dd12747103fdf2b8a5b33c04c8a39
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-03-26 02:20:17 -0700

    update

--- leetcode/memoize.js
+++ leetcode/memoize.js
@@ -1,14 +1,33 @@
+/* 
+Given a function fn, return a memoized version of that function.
+
+A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+sum accepts two integers a and b and returns a + b.
+fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+
+*/
+
 /**
  * @param {Function} fn
  * @return {Function}
  */
 function memoize(fn) {
-    
+    let lastValue = 1;
+    let result = 0;
     return function(...args) {
-        
+      if (lastValue !== result) {
+        result = fn(...args);
+        lastValue = result;
+        return fn(...args);
+      } else {
+        return lastValue;
+      }
     }
 }
-
 
 /** 
  * let callCount = 0;
